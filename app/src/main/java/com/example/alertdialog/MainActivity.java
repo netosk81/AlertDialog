255c31165efa635bf007b0d5a76473875a74ea91
netosk81 <hk.neto@gmail.com>
2023-03-05 16:32:24 -0300

    Initial commit

---
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirDialog(View view){
+
+        //Instanciar AlertDialog
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        //configurar titulo e mensagem
+        dialog.setTitle("Titulo da dialog");
+        dialog.setMessage("Mensagem da dialog");
+
+        //Configurar acoes para o sim e nao
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "Executar ação ao clicar no botão sim", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "Executar ação ao clicar no botão não", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Criar e exibir AlertDialog
+        dialog.create();
+        dialog.show();
+
+
+    }
+}
